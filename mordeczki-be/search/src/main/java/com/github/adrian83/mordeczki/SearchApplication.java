95165ad00b38eb67949185d67c36f07e3478fa4f
adrian83 <adrianbrzoza1983@gmail.com>
2021-01-23 17:09:46 +0100

    initial

---
+++ mordeczki-be/search/src/main/java/com/github/adrian83/mordeczki/SearchApplication.java
@@ -0,0 +1,13 @@
+package com.github.adrian83.mordeczki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchApplication.class, args);
+	}
+
+}
