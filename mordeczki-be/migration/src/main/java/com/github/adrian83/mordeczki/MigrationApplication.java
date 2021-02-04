139afb1c24d5015a468dc9426369a0371be25808
adrian83 <adrianbrzoza1983@gmail.com>
2021-02-04 19:02:29 +0100

    [be] closing migration app after db migration

--- mordeczki-be/migration/src/main/java/com/github/adrian83/mordeczki/MigrationApplication.java
+++ mordeczki-be/migration/src/main/java/com/github/adrian83/mordeczki/MigrationApplication.java
@@ -1,13 +1,22 @@
 package com.github.adrian83.mordeczki;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MigrationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MigrationApplication.class, args);
-	}
+  public static void main(String[] args) {
+    // SpringApplication.run(MigrationApplication.class, args);
 
+    ConfigurableApplicationContext ctx =
+        new SpringApplicationBuilder(MigrationApplication.class).web(WebApplicationType.NONE).run();
+
+    int exitCode = SpringApplication.exit(ctx, () -> 0);
+
+    System.exit(exitCode);
+  }
 }
